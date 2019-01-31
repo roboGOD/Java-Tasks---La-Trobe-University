855781a5597aad8abc55743af02d4c077dd85d3a
Ishwer Kumar <ishwerkumar666@gmail.com>
2019-01-31 17:29:33 +0530

    Added comments for Task 3

--- Task 3/Planets2.java
+++ Task 3/Planets2.java
@@ -27,34 +27,36 @@ class Planets2 {
 				ArrayList<String> outputs = new ArrayList<String>();
 				
 				while(flag) {
-					f.seek(0);
-					outputs.clear();
+					f.seek(0); // Move the pointer at the begining of the file
+					outputs.clear(); // Empty the ArrayList
 
 					int choice;
 					System.out.print("Main Menu\n1. Show Planet Connections\n2. Exit\nEnter choice >> ");
 					choice = Integer.parseInt(sc.nextLine());
 					
 					switch(choice) {
-						case 1:
+						case 1: // Show planet connections
 						System.out.print("Enter the name of the star: ");
-						sname = sc.nextLine();
+						sname = sc.nextLine(); // Name of the star
 						
 						String s;
-						while((s=f.readLine()) != null) {
-							String sarr[] = s.split(";");
-							if(sarr[0].equalsIgnoreCase(sname)) {
-								outputs.add(sarr[2]+"\t"+sarr[3]);
+						while((s=f.readLine()) != null) { // Keep reading file line by line
+							String sarr[] = s.split(";"); // Split the line by the delimiter ';'
+							if(sarr[0].equalsIgnoreCase(sname)) { // Check if the first star name matches with the given one
+								outputs.add(sarr[2]+"\t"+sarr[3]); // Add to output list
 								shr = sarr[1];
 							}
-							else if(sarr[2].equalsIgnoreCase(sname)) {
-								outputs.add(sarr[0]+"\t"+sarr[1]);
+							else if(sarr[2].equalsIgnoreCase(sname)) { // Check if the second star name matches with the given one
+								outputs.add(sarr[0]+"\t"+sarr[1]); // Add to output list
 								shr = sarr[3];
 							}
 						}
+
+						// If no match found, output list size will be 0
 						if(outputs.size() == 0) {
 							System.out.println("\nNo match found for "+sname+"\n");
 						}
-						else {
+						else { // Else print all the outputs
 							System.out.println("\nMatches for "+sname+" ("+shr+") are:");
 							for(String name : outputs)
 								System.out.println("\t"+name);
@@ -62,12 +64,12 @@ class Planets2 {
 						}
 						break;
 
-						case 2:
+						case 2: // Exit
 						flag = false;
 						System.out.println("Closing...");
 						break;
 
-						default:
+						default: // Invalid option
 						System.out.println("The option entered is incorrect.");
 					}
 				}

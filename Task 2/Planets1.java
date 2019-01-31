a9f035661cd377d60d7b7fe45d94474fefea7494
Ishwer Kumar <ishwerkumar666@gmail.com>
2019-01-31 17:32:41 +0530

    Added comments for Task 2

--- Task 2/Planets1.java
+++ Task 2/Planets1.java
@@ -8,25 +8,28 @@ class Planets1 {
 		try {
 			Scanner sc = new Scanner(System.in);
 
+			// Get filename and open the file.
 			System.out.print("Enter file name: ");
 			String filename = sc.nextLine();
 
 			FileReader ff = new FileReader(filename);
 			BufferedReader f = new BufferedReader(ff);
 			
+			// Get HR Code
 			System.out.print("Enter HR Code: ");
 			String code = sc.nextLine();
 
 			String s;
 			boolean notFound = true;
-			while((s=f.readLine())!=null) {
-				String sarr[] = s.split(";");
-				if(sarr[2].equalsIgnoreCase(code)) {
+			while((s=f.readLine())!=null) { // Read the file line by line.
+				String sarr[] = s.split(";"); // Split the line by ';'
+				if(sarr[2].equalsIgnoreCase(code)) { // Check if HR Code matches with the given one.
 					notFound = false;
-					System.out.println("\""+sarr[2]+"\""+" is "+sarr[0]+" also known as "+sarr[1]+".");
+					System.out.println("\""+sarr[2]+"\""+" is "+sarr[0]+" also known as "+sarr[1]+"."); // print the match
+					break;
 				}
 			}
-			if(notFound)
+			if(notFound) // If no match found
 				System.out.println("No match found for HR code "+"\""+code+"\"");
 		}
 		catch(Exception e) {
